e50b27e5c103329f31bea68b499d4cc05dc57ded
Chavez <nailsbrokeandfell@gmail.com>
2013-10-25 10:17:06 -0700

    gofmt love

--- dtl.go
+++ dtl.go
@@ -1,129 +1,129 @@
 package dtl
 
 import (
-    "bufio"
-    "math"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type DT struct {
-    Labels []string
-    Examples [][]float64
-    Default bool
+	Labels   []string
+	Examples [][]float64
+	Default  bool
+	T        *Tree
 }
 
 type Tree struct {
-    root Node
+	root Node
 }
 
 type Node struct {
-    name string
-    children []Node
-    examples [][]float64
+	name     string
+	children []Node
+	examples [][]float64
 }
 
-
 func information(x float64, y float64) float64 {
-    if x <= 0.0 {
-        x = 0.0000001
-    }
-    if y <= 0.0 {
-        y = 0.0000001
-    }
-    return math.Abs(-x * math.Log2(x) - y * math.Log2(y))
+	if x <= 0.0 {
+		x = 0.0000001
+	}
+	if y <= 0.0 {
+		y = 0.0000001
+	}
+	return math.Abs(-x*math.Log2(x) - y*math.Log2(y))
 }
 
 func gain(total float64, examples [][]float64) float64 {
-    var good, bad float64 = 0.0, 0.0
-    if total <= 0.0 {
-        return 0.0
-    }
-    length := 0
-    if len(examples) > 0 {
-        length = len(examples[0])
-    }
-    if length <= 0 {
-        return 0.0
-    }
-    for _, ex := range examples {
-        last := ex[length - 1]
-        if last == 1.0 {
-            good++
-        } else {
-            bad++
-        }
-    }
-    count := good + bad
-    factor := count / total
-    x := good / count
-    y := bad / count
+	var good, bad float64 = 0.0, 0.0
+	if total <= 0.0 {
+		return 0.0
+	}
+	length := 0
+	if len(examples) > 0 {
+		length = len(examples[0])
+	}
+	if length <= 0 {
+		return 0.0
+	}
+	for _, ex := range examples {
+		last := ex[length-1]
+		if last == 1.0 {
+			good++
+		} else {
+			bad++
+		}
+	}
+	count := good + bad
+	factor := count / total
+	x := good / count
+	y := bad / count
 
-    return factor * information(x, y)
+	return factor * information(x, y)
 }
 
-func entropy(totalExamples float64, node Node) float64{
-    var gains []float64
-    for _, child := range node.children {
-        gains = append(gains, gain(totalExamples, child.examples))
-    }
-    sum := float64(0.0)
-    for _, val := range gains {
-        sum += val
-    }
-    return float64(1.0 - sum)
+func entropy(totalExamples float64, node Node) float64 {
+	var gains []float64
+	for _, child := range node.children {
+		gains = append(gains, gain(totalExamples, child.examples))
+	}
+	sum := float64(0.0)
+	for _, val := range gains {
+		sum += val
+	}
+	return float64(1.0 - sum)
 }
 
 func LoadExamples(filepath string) ([][]float64, []string) {
-    file, _ := os.Open(filepath)
-    scanner := bufio.NewScanner(file)
-    var labels []string
-    examples := [][]float64{}
-    for scanner.Scan() {
-        if len(labels) == 0 {
-            labels = strings.Split(strings.Replace(scanner.Text(), "\"", "", -1), ",")
-            labels = labels[:len(labels)-1]
-            continue
-        }
-        ex := strings.Split(scanner.Text(), ",")
-        converted := []float64{}
-        for _, val := range ex {
-            f, err := strconv.ParseFloat(val, 64)
-            if err != nil {
-                panic(err)
-            }
-            converted = append(converted, f)
-        }
-        examples = append(examples, converted)
-    }
+	file, _ := os.Open(filepath)
+	scanner := bufio.NewScanner(file)
+	var labels []string
+	examples := [][]float64{}
+	for scanner.Scan() {
+		if len(labels) == 0 {
+			labels = strings.Split(strings.Replace(scanner.Text(), "\"", "", -1), ",")
+			labels = labels[:len(labels)-1]
+			continue
+		}
+		ex := strings.Split(scanner.Text(), ",")
+		converted := []float64{}
+		for _, val := range ex {
+			f, err := strconv.ParseFloat(val, 64)
+			if err != nil {
+				panic(err)
+			}
+			converted = append(converted, f)
+		}
+		examples = append(examples, converted)
+	}
 
-    if err := scanner.Err(); err != nil {
-        panic(err)
-    }
-    return examples, labels
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return examples, labels
 }
 
 func (dt *DT) Majority() (result bool) {
-    total := len(dt.Examples)
-    if total == 0 {
-        return dt.Default
-    }
-    good := 0
-    bad := 0
-    for _, ex := range dt.Examples {
-        if ex[len(ex) - 1] == 1.0 {
-            good++
-        } else {
-            bad++
-        }
-    }
-    if good > (total / 2) {
-        result = true 
-    } else if bad > (total / 2) {
-        result = false
-    } else {
-       result = dt.Default 
-    }
-    return
+	total := len(dt.Examples)
+	if total == 0 {
+		return dt.Default
+	}
+	good := 0
+	bad := 0
+	for _, ex := range dt.Examples {
+		if ex[len(ex)-1] == 1.0 {
+			good++
+		} else {
+			bad++
+		}
+	}
+	if good > (total / 2) {
+		result = true
+	} else if bad > (total / 2) {
+		result = false
+	} else {
+		result = dt.Default
+	}
+	return
 }
--- dtl_test.go
+++ dtl_test.go
@@ -7,107 +7,104 @@ import (
 
 var _ = Describe("Dtl", func() {
 
-    Describe("information", func(){
-        
-        It("returns zero if x and y are 1.0", func(){
-            Expect(information(1.0, 1.0)).To(Equal(0.0))
-        })
+	Describe("information", func() {
 
-        It("returns close to zero if x and y are 0.0", func(){
-            var expected float64 = 4.650699332842307e-06
-            Expect(information(0.0, 0.0)).To(Equal(expected))
-        })
-        
-        It("returns expected calculation", func(){
-            var expected float64 = 0.9643856189774724
-            Expect(information(0.5, 0.2)).To(Equal(expected))
-        })
-    })
+		It("returns zero if x and y are 1.0", func() {
+			Expect(information(1.0, 1.0)).To(Equal(0.0))
+		})
 
+		It("returns close to zero if x and y are 0.0", func() {
+			var expected float64 = 4.650699332842307e-06
+			Expect(information(0.0, 0.0)).To(Equal(expected))
+		})
 
-    Describe("gain", func(){
+		It("returns expected calculation", func() {
+			var expected float64 = 0.9643856189774724
+			Expect(information(0.5, 0.2)).To(Equal(expected))
+		})
+	})
 
+	Describe("gain", func() {
 
-        It("returns 0 if total <= 0", func(){
-            examples := [][]float64{}
-            Expect(gain(-1.0, examples)).To(Equal(0.0))
-            Expect(gain(0.0, examples)).To(Equal(0.0))
-        })
+		It("returns 0 if total <= 0", func() {
+			examples := [][]float64{}
+			Expect(gain(-1.0, examples)).To(Equal(0.0))
+			Expect(gain(0.0, examples)).To(Equal(0.0))
+		})
 
-        It("returns 0 if no examples", func(){
-            examples := [][]float64{}
-            Expect(gain(100.0, examples)).To(Equal(0.0))
-        })
+		It("returns 0 if no examples", func() {
+			examples := [][]float64{}
+			Expect(gain(100.0, examples)).To(Equal(0.0))
+		})
 
-        It("caclulated correctly", func(){
-            total := float64(3.0)
-            examples := [][]float64{ []float64{1.0, 1.0}, []float64{2.0, 1.0}, []float64{3.0, 0.0} }
-            expected := float64(0.9182958340544896)
-            Expect(gain(total, examples)).To(Equal(expected))
-        })
-    })
+		It("caclulated correctly", func() {
+			total := float64(3.0)
+			examples := [][]float64{{1.0, 1.0}, {2.0, 1.0}, {3.0, 0.0}}
+			expected := float64(0.9182958340544896)
+			Expect(gain(total, examples)).To(Equal(expected))
+		})
+	})
 
-    Describe("entropy", func(){
+	Describe("entropy", func() {
 
-        var (
-            examples [][]float64
-            children []Node
-            node Node
-        )
+		var (
+			examples [][]float64
+			children []Node
+			node     Node
+		)
 
-        BeforeEach(func(){
-            examples = [][]float64{ []float64{1.0, 1.0}, []float64{2.0, 1.0}, []float64{3.0, 0.0} }
-            children = []Node { Node{ name: "test2", examples: examples } }
-            node = Node{ name: "test", examples: examples, children: children}
- 
-        })
+		BeforeEach(func() {
+			examples = [][]float64{{1.0, 1.0}, {2.0, 1.0}, {3.0, 0.0}}
+			children = []Node{{name: "test2", examples: examples}}
+			node = Node{name: "test", examples: examples, children: children}
 
-        
-        It("calculates if remaining examples", func(){
-            expected := float64(0.08170416594551044) 
-            Expect(entropy(float64(3.0), node)).To(Equal(expected))
-        })
+		})
 
-        It("calculates if subset of total examples", func(){
-            expected := float64(0.7245112497836532)
-            Expect(entropy(float64(10.0), node)).To(Equal(expected))
-        })
-    })
+		It("calculates if remaining examples", func() {
+			expected := float64(0.08170416594551044)
+			Expect(entropy(float64(3.0), node)).To(Equal(expected))
+		})
 
-    Describe("LoadExamples", func(){
-    
-        It("loads in examples from csv", func(){
-            examples, labels := LoadExamples("./examples.csv")
-            Expect(labels).To(HaveLen(4))
-            Expect(examples).To(HaveLen(4))
-        })
-    })
+		It("calculates if subset of total examples", func() {
+			expected := float64(0.7245112497836532)
+			Expect(entropy(float64(10.0), node)).To(Equal(expected))
+		})
+	})
 
-    Describe("Majority", func(){
-   
-        var (
-            dt *DT
-            examples [][]float64
-        )
+	Describe("LoadExamples", func() {
 
-        BeforeEach(func(){
-            dt = &DT{Default: true}
-            examples = [][]float64{[]float64{0.0, 1.0}, []float64{0.0, 0.0}, []float64{0.0, 0.0}}
-        })
+		It("loads in examples from csv", func() {
+			examples, labels := LoadExamples("./examples.csv")
+			Expect(labels).To(HaveLen(4))
+			Expect(examples).To(HaveLen(4))
+		})
+	})
 
-        It("returns defualt if no examples", func(){
-            Expect(dt.Majority()).To(Equal(dt.Default))
-        })
+	Describe("Majority", func() {
 
-        It("returns clear majority", func(){
-            dt.Examples = examples
-            Expect(dt.Majority()).To(BeFalse())
-        })
+		var (
+			dt       *DT
+			examples [][]float64
+		)
 
-        It("returns default if results are even", func(){
-            dt.Examples = append(examples, []float64{1.0, 1.0})
-            Expect(dt.Majority()).To(Equal(dt.Default))
-        })
-    })
+		BeforeEach(func() {
+			dt = &DT{Default: true}
+			examples = [][]float64{{0.0, 1.0}, {0.0, 0.0}, {0.0, 0.0}}
+		})
+
+		It("returns defualt if no examples", func() {
+			Expect(dt.Majority()).To(Equal(dt.Default))
+		})
+
+		It("returns clear majority", func() {
+			dt.Examples = examples
+			Expect(dt.Majority()).To(BeFalse())
+		})
+
+		It("returns default if results are even", func() {
+			dt.Examples = append(examples, []float64{1.0, 1.0})
+			Expect(dt.Majority()).To(Equal(dt.Default))
+		})
+	})
 
 })
